a63d9a206430675b0b7e0879d94660d7e9dbe499
Davin <286156958+nivad320-lgtm@users.noreply.github.com>
2026-06-30 18:14:51 +0900

    Add tab switch feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import "./styles.css";
 import DisplayController from "./homepage.js";
+import AboutPage from "./about.js"
 
+const indexController = {
+    tabSwitch() {
+        const homeTab = document.querySelector('#home');
+        const aboutTab = document.querySelector('#about');
 
-DisplayController.loadHome();
-console.log("test");
+        const contentContainer = document.querySelector('#content');
+        DisplayController.loadHome()
+        let currentTab = 'homeTab';
+
+        homeTab.addEventListener('click', (e) => {
+            if (currentTab !== 'home') {
+                contentContainer.replaceChildren();
+                DisplayController.loadHome();
+                currentTab = 'home';
+            }
+        })        
+        aboutTab.addEventListener('click', (e) => {
+            if (currentTab !== 'about') {
+                contentContainer.replaceChildren();
+                AboutPage.loadHome();
+                currentTab = 'about';
+            }
+        })
+    }
+}
+
+indexController.tabSwitch();
